8bb4c5eded4c51add4f91cd734a3023638cc164b
James Gomez <jim.gomez.48@gmail.com>
2014-03-24 13:47:46 -0700

    Added documentation to CostMap class

--- src/livewire/CostMap.java
+++ src/livewire/CostMap.java
@@ -1,13 +1,17 @@
 package livewire;
 
-import com.googlecode.javacv.cpp.opencv_core;
 import com.googlecode.javacv.cpp.opencv_core.CvMat;
-import com.googlecode.javacv.cpp.opencv_highgui;
+import com.googlecode.javacv.cpp.opencv_core.CvPoint;
 
 import java.util.*;
 
 /**
- * WRITE CLASS DESCRIPTION HERE
+ * CostMap is used to calculate and store the cumulative costs of the image pixels
+ * from a given seedpoint. It uses a variation of Dijkstra's shortest path algorithm
+ * to expand the image pixel graph and calculate least cost paths from the seed point
+ * to all other pixels in the image.
+ *
+ * @author James Gomez
  */
 public class CostMap
 {
@@ -16,34 +20,55 @@ public class CostMap
     Node[][] costs;
     Node current;
 
+    /**
+     * Used for the expansion algorithm to store cumulative costs and parent
+     * pointers
+     */
     public class Node
     {
+        /** the row of the pixel that this node corresponds to */
         public short row;
+        /** the column of the pixel that this node corresponds to */
         public short col;
+        /** the current cumulative cost of this node from the seed point */
         public int cost;
+        /** the next node along the lowest cost path from this node to the seed */
         public Node parent;
 
-        public boolean equals(Node n) {
+        @Override
+        public boolean equals(Object o)
+        {
+            if (o == null || !(o instanceof Node))
+                return false;
+            if (o == this)
+                return true;
+
+            Node n = (Node) o;
+
             return n.row == this.row && n.col == this.col;
         }
 
         @Override
-        public String toString() {
+        public String toString()
+        {
             String parent = "set";
             if (parent == null)
                 parent = "null";
 
-            return "Node[row:" +row + " col:" + col + " cost:" + cost +
+            return "Node[row:" + row + " col:" + col + " cost:" + cost +
                     " parent:" + parent + "]";
         }
     }
 
-
-    public CostMap(CvMat image) {
+    /** Creates a CostMap with the given CvMat image as its starting data */
+    public CostMap(CvMat image)
+    {
         reset(image);
     }
 
-    public void reset(CvMat image) {
+    /** Resets the algorithm to a non-expanded state using a new image */
+    public void reset(CvMat image)
+    {
         original = new Node[image.rows()][image.cols()];
         costs = new Node[image.rows()][image.cols()];
 
@@ -62,7 +87,9 @@ public class CostMap
         reset();
     }
 
-    public void reset() {
+    /** Resets the algorithm to the original, non-expanded state. */
+    public void reset()
+    {
         //make a deep copy of the original array of nodes
         for (int i = 0; i < original.length; i++) {
             for (int j = 0; j < original[0].length; j++) {
@@ -76,22 +103,57 @@ public class CostMap
         }
     }
 
-    private enum ExpandMethod { COST, DIST }
+    private enum ExpandMethod
+    {
+        COST, DIST
+    }
 
-    public Node addSeed(int row, int col) {
+    /**
+     * Sets a seedpoint as the starting point of the expansion algorithm. A call to
+     * this method triggers the expansion algorithm to run, generating cumulative
+     * costs and parents pointers.
+     *
+     * @param seed the starting seedpoint
+     */
+    public void addSeed(CvPoint seed)
+    {
+        addSeed(seed.y(), seed.x());
+    }
+
+    /**
+     * Sets a seedpoint as the starting point of the expansion algorithm. A call to
+     * this method triggers the expansion algorithm to run, generating cumulative
+     * costs and parents pointers.
+     *
+     * @param row the pixel row of the starting seedpoint
+     * @param col the column of the starting seedpoint
+     */
+    public void addSeed(int row, int col)
+    {
         //TODO keep track of seed points for path cooling
         System.out.println("New seed-point row:" + row + " col:" + col);
         reset();
         expand(row, col, ExpandMethod.COST);
+    }
 
+    /** @return the Node corresponding to the specified point in the image */
+    public Node getNode(CvPoint point)
+    {
+        return getNode(point.y(), point.x());
+    }
+
+    /** @return the Node corresponding to the specified point in the image */
+    public Node getNode(int row, int col)
+    {
         return costs[row][col];
     }
 
-    public Node getNode(int row, int col){
-        return costs[row][col];
-    }
-
-    private void expand(int row, int col, ExpandMethod method) {
+    /**
+     * Generates cumulative costs and parent pointers using a variation of Dijkstra's
+     * shortest path algorithm. The resultant min-cost tree is stored in a 2-D array
+     */
+    private void expand(int row, int col, ExpandMethod method)
+    {
         //create algorithm data structures COMPARE VIA CUM COST
         Set<Node> closed = new HashSet<Node>();
         PriorityQueue<Node> wavefront;
@@ -101,7 +163,8 @@ public class CostMap
                 wavefront = new PriorityQueue<Node>(1000, new Comparator<Node>()
                 {
                     @Override
-                    public int compare(Node a, Node b) {
+                    public int compare(Node a, Node b)
+                    {
                         if (a.cost > b.cost) return 1;
                         if (a.cost < b.cost) return -1;
                         return 0;
@@ -112,7 +175,8 @@ public class CostMap
                 wavefront = new PriorityQueue<Node>(1000, new Comparator<Node>()
                 {
                     @Override
-                    public int compare(Node a, Node b) {
+                    public int compare(Node a, Node b)
+                    {
                         double distA = euclideanDist(current, a);
                         double distB = euclideanDist(current, b);
                         if (distA > distB)
@@ -123,7 +187,7 @@ public class CostMap
                 });
                 break;
             default:
-                System.out.println("Invalid method argument for CostMap.expand()");
+                System.err.println("Invalid method argument for CostMap.expand()");
                 return;
         }
 
@@ -168,7 +232,9 @@ public class CostMap
         System.out.println("Expanding: 100%");
     }
 
-    private ArrayList<Node> getNeighbors(Node n) {
+    /** @return a list of n's neighbor Nodes */
+    private ArrayList<Node> getNeighbors(Node n)
+    {
         ArrayList<Node> neighbors = new ArrayList<Node>(8);
 
         for (int i = n.row - 1; i <= n.row + 1; i++) {
@@ -185,7 +251,9 @@ public class CostMap
         return neighbors;
     }
 
-    private int euclideanAdd(Node current, Node n) {
+    /** @return the euclidean-scaled cumulative cost from Node current to Node n */
+    private int euclideanAdd(Node current, Node n)
+    {
         //if diagonal, scale by RAD2
         if (current.row != n.row && current.col != n.col)
             return current.cost + (int) (RAD2 * costs[n.row][n.col].cost);
@@ -193,7 +261,9 @@ public class CostMap
         return current.cost + costs[n.row][n.col].cost;
     }
 
-    private double euclideanDist(Node a, Node b) {
+    /** @return the euclidean distance between Node a and Node b */
+    private double euclideanDist(Node a, Node b)
+    {
         return (double) Math.sqrt(((b.row - a.row) * (b.row - a.row)) +
                 ((b.col - a.col) * (b.col - a.col)));
     }
